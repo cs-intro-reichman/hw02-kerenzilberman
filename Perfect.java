932099b1ac92aa62760795626b3866faf9e5f00f
kerenzilberman <154364524+kerenzilberman@users.noreply.github.com>
2023-12-28 22:24:49 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -4,5 +4,21 @@
 public class Perfect {
 	public static void main (String[] args) {
 		//// Put your code here
+   int number = Integer.parseInt (args[0]);
+   String answer = (number + " is a perfect number since " + number + " = 1");
+   int add = 1;
+   
+   for (int i = 2; i < number; i++){
+     if (number % i == 0) {
+	     add+= i;
+		 answer+= " + " + i;
+	 }
+   }
+	if (add == number){
+	  System.out.println (answer);
+	 }
+	 else {
+	  System.out.println (number + " is not a perfect number");
+	 }
 	}
 }
